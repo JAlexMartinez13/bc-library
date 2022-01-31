77795f1f1b61023593afc3f5af57c81d0ce3d277
Alex Martinez <j.alexandermartinez@Alexs-MacBook-Air.local>
2022-01-31 17:23:40 -0500

    created rep, firestore, express, postman

--- src/index.js
+++ src/index.js
@@ -1,26 +1,59 @@
 const express = require('express');
+const { initializeApp, cert } = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore");
 
-const app = express()
+const credentials = require("../credentials.json");
 
+initializeApp({
+    credential: cert(credentials),
+})
+
+const db = getFirestore();
+
+
+const app = express();
 app.use(express.json());
 
 
-app.get('/', (req, res) => {   // req = require & res = response
-    res.send('Hello World!')
+app.get('/', (request, response) => {
+    const userCollection = db.collection("users");
+
+    userCollection
+    .get()
+    .then(snapshot => {
+
+        const users = []
+        snapshot.forEach(doc => {
+            users.push({id: doc.id, ...doc.data()})
+        })
+
+        response.send(users)
+    })
+
+    // response.send('Hello World!');
 })
 
+app.post('/users', (req, res) => {
+    const {name, age, email} = req.body;
+    
+    // const {name} = req.body;
+    // const name = req.body.name;
 
-app.post('/users', (req, res) =>{
-    const {name, age, email} = req.body
+    const user = { name, age, email };
 
-    const user = {name, age, email}
+    // const user = {
+    //     fullName: name, 
+    //     age: age,
+    //     email: email
+    // }
 
     const result = `My name is ${user.name}, I am ${user.age} years old and my email is ${user.email}`;
 
-    res.send(result)
+    res.send(result);
 })
 
 
+
 app.listen(3000, () => {
-    console.log('We be listening on 3000')
-})  
+    console.log('We be listening on 3000');
+})
